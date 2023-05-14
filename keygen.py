302312d6439e78227fe03e3a358231fee59a73b4
Khaled Korfali <46685494+KhaledKorfali2@users.noreply.github.com>
2023-05-14 11:58:01 -0700

    keygen.py now creates subdirectories if they don't exist instead of simply printing an error

--- keygen.py
+++ keygen.py
@@ -9,6 +9,13 @@ import os
 import sys
 from Crypto.PublicKey import RSA
 
+
+# Generate a 2048-bit RSA key pair for the sender
+sender_key = RSA.generate(2048)
+
+# Generate a 2048-bit RSA key pair for the receiver
+receiver_key = RSA.generate(2048)
+
 #Define the name of the main director folder
 main_dir = os.getcwd()
 
@@ -22,24 +29,22 @@ subdir_path_sender = os.path.join(os.getcwd(), sender_dir)
 # Get the full path of the subdirectory
 subdir_path_sender_keys = os.path.join(subdir_path_sender, sender_keys_dir)
 
-# Check if the subdirectory exists
-if os.path.isdir(subdir_path_sender):
-    # Check if the subdirectory exists
+
+# Check if the subdirectory does not exist
+if not os.path.isdir(subdir_path_sender):
+    os.mkdir(subdir_path_sender)
     os.chdir(subdir_path_sender)
-    if os.path.isdir(subdir_path_sender_keys):
-        os.chdir(subdir_path_sender_keys)
-        # Generate a 2048-bit RSA key pair for the sender
-        sender_key = RSA.generate(2048)
-        with open("sender_private_key.pem", "wb") as f:
-            f.write(sender_key.export_key())
-        with open("sender_public_key.pem", "wb") as f:
-            f.write(sender_key.publickey().export_key())
-    else:
-        # If the subdirectory does not exist, print an error message
-        print(f"Error: {subdir_path_sender_keys} directory does not exist.")
-else:
-    # If the subdirectory does not exist, print an error message
-    print(f"Error: {subdir_path_sender} directory does not exist.")
+if not os.path.isdir(subdir_path_sender_keys):
+    os.mkdir(subdir_path_sender_keys)
+    os.chdir(subdir_path_sender_keys)
+    
+with open("sender_private_key.pem", "wb") as f:
+    f.write(sender_key.export_key())
+with open("sender_public_key.pem", "wb") as f:
+    f.write(sender_key.publickey().export_key())
+with open("receiver_public_key.pem", "wb") as f:
+    f.write(receiver_key.publickey().export_key())
+
 
 #Go back to main directory folder
 os.chdir(main_dir)
@@ -55,26 +60,22 @@ subdir_path_receiver = os.path.join(os.getcwd(), receiver_dir)
 # Get the full path of the subdirectory
 subdir_path_receiver_keys = os.path.join(subdir_path_receiver, receiver_keys_dir)
 
-# Check if the subdirectory exists
-if os.path.isdir(subdir_path_receiver):
-    # Check if the subdirectory exists
+
+
+# Check if the subdirectory does not exist
+if not os.path.isdir(subdir_path_receiver):
+    os.mkdir(subdir_path_receiver)
     os.chdir(subdir_path_receiver)
-    if os.path.isdir(subdir_path_receiver_keys):
-        os.chdir(subdir_path_receiver_keys)
-        # Generate a 2048-bit RSA key pair for the receiver
-        receiver_key = RSA.generate(2048)
-        with open("receiver_private_key.pem", "wb") as f:
-            f.write(receiver_key.export_key())
-        with open("receiver_public_key.pem", "wb") as f:
-            f.write(receiver_key.publickey().export_key())
-    else:
-        # If the subdirectory does not exist, print an error message
-        print(f"Error: {subdir_path_receiver_keys} directory does not exist.")
-else:
-    # If the subdirectory does not exist, print an error message
-    print(f"Error: {subdir_path_receiver} directory does not exist.")
-
-
+if not os.path.isdir(subdir_path_receiver_keys):
+    os.mkdir(subdir_path_receiver_keys)
+    os.chdir(subdir_path_receiver_keys)
+    
+with open("receiver_private_key.pem", "wb") as f:
+    f.write(receiver_key.export_key())
+with open("receiver_public_key.pem", "wb") as f:
+    f.write(receiver_key.publickey().export_key())
+with open("sender_public_key.pem", "wb") as f:
+    f.write(sender_key.publickey().export_key())
 
 print("RSA Keys Generated!!!")
 sys.exit(0)
